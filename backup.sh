292d85649e0183bff460ae2ed96f4f0d822b1fb1
DeployedReject <mishravaibhav2048@gmail.com>
2026-04-15 22:19:39 +0530

    Script done

---
+++ backup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# --- Variables ---
+SOURCE_FOLDER="world"
+TARGET_FOLDER="backup"
+LOG_FILE="backup.log"
+STATUS_FILE="backup.status.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+# --- Main Logic ---
+trap ./svctrl.sh -mc save-on EXIT
+
+
+
+
+# [ -d ... ] checks if the path exists and is a directory
+if [ -d "$SOURCE_FOLDER" ]; then
+  echo "Success: The folder '$SOURCE_FOLDER' exists in the current directory." | tee -a "$LOG_FILE" > "$STATUS_FILE"
+#--Preventing race condition.
+  ./svctrl.sh -mc save-all
+  ./svctrl.sh -mc save-off
+#--Checking if this is the first time the backup is being made
+  if[! -d "$TARGET_FOLDER"]; then
+   mkdir $TARGET_FOLDER
+  fi
+ mkdir "$TARGET_FOLDER/$TIMESTAMP.backup"
+ cp -r world/. "$TARGET_FOLDER/$TIMESTAMP.backup"
+ cd $TARGET_FOLDER
+ COUNT=$(ls -1d *.backup 2>/dev/null | wc -l)
+ if ["$COUNT" -gt 3]; then
+  OLDEST=$(ls -1d *.backup | sort | head -n 1)
+  rm -rf "$OLDEST"
+  echo "Deleted $OLDEST (Total: $COUNT)" | tee -a "$LOG_FILE" >> STATUS_FILE
+ fi
+cd .. 
+rclone sync $TARGET_FOLDER gdrive:$TARGET_FOLDER
+
+
+else
+  echo "Error: Could not find a folder named '$TARGET_FOLDER'." >> "$LOG_FILE"
+  echo "Current directory contents:"
+  ls -F
+  exit 1
+fi
+
+exit 0
